3c64e0bfa39b5c0ffc24098c74b002ed45e02f22
jatin dhamija <jdhammija816@gmail.com>
2024-10-15 15:21:28 +0530

    ubuntu font

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,27 +2,35 @@ const About = () => {
   return (
     <div className="bg-gray-800 min-h-screen flex items-center justify-center">
       <div className="p-10 md:max-w-screen-lg">
-        <p className="text-gray-50 text-2xl  font-semibold py-10">About</p>
-        <div className="text-gray-300 font-mono">
-          <p className=" py-2">
-            Hi, I’m Jatin Dhamija, a Full Stack Developer with a passion for
-            creating modern, scalable web and mobile applications. My expertise
-            includes working with technologies like React, React Native,
-            Node.js, Express, MongoDB, Firebase, TypeScript, Tailwind CSS.
+        <p className="text-gray-50 text-2xl font-semibold py-10">About</p>
+        <div className="text-gray-300  font-ubuntu">
+          <p className="py-2">
+            Hello! I’m Jatin Dhamija, a dedicated Full Stack Developer with a
+            strong passion for crafting modern, scalable web and mobile
+            applications. I specialize in using technologies such as React,
+            React Native, Node.js, Express, MongoDB, Firebase, TypeScript, and
+            Tailwind CSS to create seamless digital experiences.
           </p>
-          <p className=" py-2">
-            I completed my B.Tech in Computer Science and Engineering from
-            Panipat Institute of Engineering and Technology and recently wrapped
-            up a 3-month internship where I developed a React Native mobile app
-            for a startup. In this role, I turned the provided Figma designs
-            into a fully functional app, focusing on development, integration of
-            backend services, and ensuring a smooth user experience.
+          <p className="py-2">
+            I hold a B.Tech in Computer Science and Engineering from Panipat
+            Institute of Engineering and Technology. Recently, I completed a
+            3-month internship where I played a key role in developing a mobile
+            application using React Native for a startup. In this project, I
+            transformed Figma designs into a fully functional app, integrating
+            backend services and ensuring an optimal user experience.
           </p>
-          <p className=" py-2">
-            I also have experience building responsive, user-friendly websites
-            and enjoy solving complex technical challenges with efficient,
-            scalable solutions. My goal is always to deliver high-quality
-            products that provide both functionality and great user experiences.
+          <p className="py-2">
+            My expertise extends to building responsive, user-friendly websites,
+            where I tackle complex challenges by implementing efficient and
+            scalable solutions. Whether it's frontend development or backend
+            architecture, I aim to deliver products that not only meet
+            functional requirements but also offer an intuitive and engaging
+            user experience.
+          </p>
+          <p className="py-2">
+            I’m constantly driven by the pursuit of learning and improving,
+            always striving to stay updated with the latest industry trends to
+            build innovative and high-quality solutions.
           </p>
         </div>
       </div>
