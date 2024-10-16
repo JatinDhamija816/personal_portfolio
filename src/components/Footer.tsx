edae8dc37a97a7c7a976cd2e417c4aff26b20c90
jatin dhamija <jdhammija816@gmail.com>
2024-10-16 16:11:00 +0530

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   return (
